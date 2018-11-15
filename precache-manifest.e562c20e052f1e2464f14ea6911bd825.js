766c5c716f75a43f3c7e4ec377f231efd1f34b14
Vyper248 <vyper375@hotmail.com>
2018-11-15 20:14:59 +0000

    Updates

---
+++ precache-manifest.e562c20e052f1e2464f14ea6911bd825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "897a1eb5f509849f95c9",
+    "url": "/image-recognition/static/js/runtime~main.897a1eb5.js"
+  },
+  {
+    "revision": "cab84d59d091e8681c87",
+    "url": "/image-recognition/static/js/main.cab84d59.chunk.js"
+  },
+  {
+    "revision": "a9ba30515902b1916f53",
+    "url": "/image-recognition/static/js/1.a9ba3051.chunk.js"
+  },
+  {
+    "revision": "cab84d59d091e8681c87",
+    "url": "/image-recognition/static/css/main.d8319d0b.chunk.css"
+  },
+  {
+    "revision": "057144799acf85dc9c033ca2a71d5c81",
+    "url": "/image-recognition/index.html"
+  }
+];
